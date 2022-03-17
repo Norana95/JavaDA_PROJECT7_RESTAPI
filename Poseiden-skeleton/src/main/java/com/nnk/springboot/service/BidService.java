65dad6cc67bbbc7ee3f6c812a2b2122bdbff39f8
Norana95 <boublenza.n@gmail.com>
2022-03-17 20:24:14 +0100

    implement CRUD in layer service

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/service/BidService.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/service/BidService.java
@@ -1,7 +1,47 @@
 package com.nnk.springboot.service;
 
+import com.nnk.springboot.domain.Bid;
+import com.nnk.springboot.repositories.BidRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class BidService {
+
+    @Autowired
+    BidRepository bidRepository;
+
+    public void createBid(Bid bid){
+        bidRepository.save(bid);
+    }
+    public void findBid(Integer id){
+        bidRepository.findById(id);
+    }
+    public void updateBid(Bid bid){
+        bid.setAccount(bid.getAccount());
+        bid.setType(bid.getType());
+        bid.setBidQuantity(bid.getBidQuantity());
+        bid.setAskQuantity(bid.getAskQuantity());
+        bid.setBid(bid.getBid());
+        bid.setAsk(bid.getAsk());
+        bid.setBenchmark(bid.getBenchmark());
+        bid.setBidListDate(bid.getBidListDate());
+        bid.setCommentary(bid.getCommentary());
+        bid.setSecurity(bid.getSecurity());
+        bid.setStatus(bid.getStatus());
+        bid.setTrader(bid.getTrader());
+        bid.setBook(bid.getBook());
+        bid.setCreationName(bid.getCreationName());
+        bid.setCreationDate(bid.getCreationDate());
+        bid.setRevisionName(bid.getRevisionName());
+        bid.setRevisionDate(bid.getRevisionDate());
+        bid.setDealName(bid.getDealName());
+        bid.setDealType(bid.getDealType());
+        bid.setSourceListId(bid.getSourceListId());
+        bid.setSide(bid.getSide());
+    }
+    public void deleteBid(Bid bid){
+        bidRepository.delete(bid);
+    }
+
 }
--- Poseiden-skeleton/src/main/java/com/nnk/springboot/service/CurvePointService.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/service/CurvePointService.java
@@ -1,7 +1,32 @@
 package com.nnk.springboot.service;
 
+import com.nnk.springboot.domain.Bid;
+import com.nnk.springboot.domain.CurvePoint;
+import com.nnk.springboot.repositories.CurvePointRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CurvePointService {
+
+    @Autowired
+    CurvePointRepository curvePointRepository;
+
+    public void createCurvePoint(CurvePoint curvePoint){
+        curvePointRepository.save(curvePoint);
+    }
+    public void findCurvePointById(Integer id){
+        curvePointRepository.findById(id);
+    }
+    public void updateCurvePoint(CurvePoint curvePoint){
+        curvePoint.setCurveId(curvePoint.getCurveId());
+        curvePoint.setAsOfDate(curvePoint.getAsOfDate());
+        curvePoint.setTerm(curvePoint.getTerm());
+        curvePoint.setValue(curvePoint.getValue());
+        curvePoint.setCreationDate(curvePoint.getCreationDate());
+    }
+    public void deleteCurvePoint(CurvePoint curvePoint){
+        curvePointRepository.delete(curvePoint);
+    }
+
 }
